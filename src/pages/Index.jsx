3c5b4662b0276f6e4cbaa0c51617d941d60dd210
DuGuYifei <liuyifei97@163.com>
2025-08-23 17:14:51 +0200

    feat: 添加简介

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -361,6 +361,20 @@ const Index = () => {
               <h3 className="text-2xl font-bold mb-6">Kontaktinformationen</h3>
               <div className="space-y-4">
                 <div className="flex items-start">
+                  <div>
+                    <p>Herzlich willkommen im China-Restaurant „Ding Shang“, einem
+                      exquisiten chinesischem Restaurant in der malerischen Nähe des
+                      Rosenheimer Platzes. Hier können Sie authentische SzechuanKüche, Huaiyang-Küche und einzigartige Fusionsgerichte
+                      genießen. Unsere Spezialitäten umfassen verführerischen
+                      Eichhörnchenfisch, köstlichen grünen Pfeffer-Barsch, geschmorte
+                      Löwenkopfklöße, knusprig-zartes süß-saures Schweinefleisch,
+                      aromatisch-kräftige Fischsuppe mit Tofu, verschiedene
+                      vegetarische Gerichte, klassische Salzwasserente und viele weitere
+                      Gerichte. Wir bieten Ihnen ein Fest für Ihren Geschmackssinn und
+                      freuen uns auf Ihren Besuch!</p>
+                  </div>
+                </div>
+                <div className="flex items-start">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-4 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
